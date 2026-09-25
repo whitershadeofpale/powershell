b82924d17b07cba5c8b5218f34001746922d8b33
whitershadeofpale <65859955+whitershadeofpale@users.noreply.github.com>
2026-09-25 18:01:41 +0300

    working version
    given a duration in days, this script calculates the uptime of a remote machine, similar to last command in linux.

--- uykuanaliz.ps1
+++ uykuanaliz.ps1
@@ -1,202 +1,190 @@
 param(
     [string]$Computername=$env:COMPUTERNAME,
-    [int]$Days=30
+    [int]$Days=7,
+    [Switch]$Details
 )
-[datetime]$scripttime = Get-Date
-[datetime]$currentTime= $scripttime
-[datetime]$previousTime=$scripttime.AddDays(-$days)
 
-Write-Host "PrevTime = $previousTime"
+[datetime]$t1 = Get-Date    # time of script run
+[datetime]$t0 = $t1.AddDays(-$days) # start of search
 
+# Getting related events
 if (Test-ICMP $Computername) {
-    $evs1 = Get-WinEvent -cn $Computername -FilterHashtable @{
+    $evs = Get-WinEvent -ComputerName $Computername -FilterHashtable @{
         Logname="System";
         ProviderName=("Microsoft-Windows-Kernel-Power","Microsoft-Windows-Power-Troubleshooter","Microsoft-Windows-Kernel-General","eventlog");
         Id=1,12,13,42,107,6005,6006,6008;
-        StartTime=$scripttime.AddDays(-$days)
-    } | Sort-Object TimeCreated
-    $diffSeconds = 10
-    $evs1 | ft -AutoSize
-    
-    Write-Host "CurrentTime $($evs1[0].TimeCreated)"
-    # ============================
-    $evs2 = $evs1 | ForEach-Object {
-        if ($_.ProviderName -eq "Microsoft-Windows-Power-Troubleshooter" -and $_.Id -eq 1) {
-            Write-Host "PT-1"
-            [datetime]$currentTime = $_.TimeCreated
-            $currentType = "Start"
+        StartTime=$t0
+    } | Sort-Object -Property TimeCreated
 
-            [int]$fark = ($currentTime - $previousTime).TotalSeconds
-            if (($fark -gt $diffSeconds) -and ($currentType -ne $previousType)) {
-                [PSCustomObject]@{
-                    Time= $currentTime
-                    Type = $currentType
-                    Id = $_.Id
-                    Fark = $fark
-                    Aynimi = ($currentType -eq $previousType)
-                }
-            }
+    # fixing time for 6008 events
+    foreach ($ev in $evs) {
+        if ($ev.Id -eq 6008) {
+            [datetime]$time6008 = Get-Date -Date ("$($ev.Properties[1].Value -replace([char]0x200e,'')) $($ev.Properties[0].Value)")
+            $ev.TimeCreated = $time6008
         }
-        elseif ($_.ProviderName -eq "eventlog" -and $_.Id -eq 6005) {
-            Write-Host "6005"
-            [datetime]$currentTime = $_.TimeCreated
-            $currentType = "Start"
-
-            [int]$fark = ($currentTime - $previousTime).TotalSeconds
-            if (($fark -gt $diffSeconds) -and ($currentType -ne $previousType)) {
-                [PSCustomObject]@{
-                    Time= $currentTime
-                    Type = $currentType
-                    Id = $_.Id
-                    Fark = $fark
-                    Aynimi = ($currentType -eq $previousType)
-                }
-            }
-        }
-        elseif ($_.ProviderName -eq "Microsoft-Windows-Kernel-General" -and $_.Id -eq 12) {
-            Write-Host "KG-12"
-            [datetime]$currentTime = $_.TimeCreated
-            $currentType = "Start"
-        
-            [int]$fark = ($currentTime - $previousTime).TotalSeconds
-            if (($fark -gt $diffSeconds) -and ($currentType -ne $previousType)) {
-                [PSCustomObject]@{
-                    Time= $currentTime
-                    Type = $currentType
-                    Id = $_.Id
-                    Fark = $fark
-                    Aynimi = ($currentType -eq $previousType)
-                }
-            }
-        }
-        elseif ($_.ProviderName -eq "Microsoft-Windows-Kernel-General" -and $_.Id -eq 13) {
-            Write-Host "KG-13"
-            [datetime]$currentTime = $_.TimeCreated
-            $currentType = "Shutdown"
-
-            [int]$fark = ($currentTime - $previousTime).TotalSeconds
-            if (($fark -gt $diffSeconds) -and ($currentType -ne $previousType)) {
-                [PSCustomObject]@{
-                    Time= $currentTime
-                    Type = $currentType
-                    Id = $_.Id
-                    Fark = $fark
-                    Aynimi = ($currentType -eq $previousType)
-                }
-            }
-        }
-        elseif ($_.ProviderName -eq "eventlog" -and $_.Id -eq 6006) {
-            Write-Host "6006"
-            [datetime]$currentTime = $_.TimeCreated
-            $currentType = "Shutdown"
-            Write-Host "current: $currentTime, previous : $previousTime"
-            [int]$fark = ($currentTime - $previousTime).TotalSeconds
-            if (($fark -gt $diffSeconds) -and ($currentType -ne $previousType)) {
-                [PSCustomObject]@{
-                    Time= $currentTime
-                    Type = $currentType
-                    Id = $_.Id
-                    Fark = $fark
-                    Aynimi = ($currentType -eq $previousType)
-                }
-            }
-        }
-        elseif ($_.ProviderName -eq "Microsoft-Windows-Kernel-Power" -and $_.Id -eq 107) {
-            Write-Host "KP-107"
-            [datetime]$currentTime = $_.TimeCreated
-            $currentType = "Shutdown"
-
-            [int]$fark = ($currentTime - $previousTime).TotalSeconds
-            if (($fark -gt $diffSeconds) -and ($currentType -ne $previousType)) {
-                [PSCustomObject]@{
-                    Time= $currentTime
-                    Type = $currentType
-                    Id = $_.Id
-                    Fark = $fark
-                    Aynimi = ($currentType -eq $previousType)
-                }
-            }
-        }
-        elseif ($_.ProviderName -eq "eventlog" -and $_.Id -eq 6008) {
-            Write-Host "6008"
-            [datetime]$currentTime = Get-Date -Date ("$($_.Properties[1].Value -replace([char]0x200e,'')) $($_.Properties[0].Value)")
-            $currentType = "Shutdown"
-
-            [int]$fark = ($currentTime - $previousTime).TotalSeconds
-    #        if (($fark -gt $diffSeconds) -and ($currentType -ne $previousType)) {
-                [PSCustomObject]@{
-                    Time= $currentTime
-                    Type = $currentType
-                    Id = $_.Id
-                    Fark = $fark
-                    Aynimi = ($currentType -eq $previousType)
-                }
-    #       }
-            # else{
-            #     Write-Host "current: $currentTime, previous : $previousTime"
-            #     Write-Host "current: $currentType, previous : $previousType"
-            # }
-        }
-        elseif ($_.ProviderName -eq "Microsoft-Windows-Kernel-Power" -and $_.Id -eq 42) {
-            Write-Host "KP-42"
-            [datetime]$currentTime = $_.TimeCreated
-            $currentType = "Shutdown"
-
-            [int]$fark = ($currentTime - $previousTime).TotalSeconds
-            if (($fark -gt $diffSeconds) -and ($currentType -ne $previousType)) {
-                [PSCustomObject]@{
-                    Time= $currentTime
-                    Type = $currentType
-                    Id = $_.Id
-                    Fark = $fark
-                    Aynimi = ($currentType -eq $previousType)
-                }
-            }
-        }
-        [datetime]$previousTime = $currentTime
-        $previousType = $currentType
-    }
-    # ============================
-
-    $evs2 += [PSCustomObject]@{
-        Time = $scripttime
-        Type = "Shutdown"
-        Id   = 0
-        Fark = [int]($scripttime - $previousTime).TotalSeconds
     }
 
-    $evs2 | ft -AutoSize
-
-    $evs2 | Out-GridView
-
-    [datetime]$previousTime=$scripttime.AddDays(-$days)
-    [string]$previousType=""
-    $totalUptime=0
-    # $prevUptime=$evs2[0].Time
-    $evs2 | Sort-Object -Property Time | ForEach-Object {
-        [int]$fark = ($_.Time - $previousTime).TotalSeconds
-        if (($fark -gt 0) -and ($_.Time -gt 0) -and ($_.Type -ne $previousType)) {
-            if ($_.Type -eq "Shutdown") {
-                $fark1 = ($_.Time - $previousTime).TotalSeconds
-                $totalUptime += $fark1
-                # Write-Host "$($_.Time) - $($_.Id) = $fark1"
-            }
-            else {
-                $prevUptime = $_.Time
-            }
+    # what is the state of the computer at t0
+    if ($Details) { $evs[0] }
+    if ($evs[0].Id -in (1,12,6005)) { # these are bootup events
+        $previous_action = "up"
+        $pseudo0 = [PSCustomObject]@{
+            TimeCreated = $t0
+            Id = 13 # pseudo down event
+            ProviderName = "Microsoft-Windows-Kernel-General"
         }
-        else {
-            "current : $($_.Time) - $($_.Type)"
-            "previous : $previousTime - $previousType"
-        }
-        $previousTime = $_.Time
     }
+    elseif ($evs[0].Id -in (13,42,107,6006,6008)) { # these are shutdown events
+        $previous_action = "down"
+        $pseudo0 = [PSCustomObject]@{
+            TimeCreated = $t0
+            Id = 12 # pseudo up event
+            ProviderName = "Microsoft-Windows-Kernel-General"
+        }
+    }
+    $pseudo1 = [PSCustomObject]@{
+            TimeCreated = $t1
+            Id = 13
+            ProviderName = "Microsoft-Windows-Kernel-General"
+        }
 
-    # $evs2 | Out-GridView
-    "{0:N1} seconds" -f $totalUptime
-    "{0:N1} hours" -f ($totalUptime/3600)
-    "{0:N1} days" -f ($totalUptime/3600/24)
+    $evs2 = ($evs | select TimeCreated, Id, ProviderName) + $pseudo0 + $pseudo1 | 
+        Where-Object {-Not (($_.ProviderName -eq "Microsoft-Windows-Kernel-General") -and ($_.Id -eq 1))} | 
+            Sort-Object -Property TimeCreated
+
+    if ($Details) {
+        $evs2 | Out-GridView
+    }
+    # Build the final array with human-readable up-down column
+    $ignore_window = 30 # seconds
+    $previous_time = $evs2[0].TimeCreated
+    $evs3 = foreach ($ev in $evs2) {
+        if ($ev.ProviderName -eq "Microsoft-Windows-Power-Troubleshooter" -and $ev.Id -eq 1) {
+            [int]$diff = ($ev.TimeCreated - $previous_time).TotalSeconds
+            if (-Not ($previous_action -eq "up" -and ($diff -le $ignore_window))) {
+                [PSCustomObject]@{
+                    TimeCreated = $ev.TimeCreated
+                    Id = $ev.Id
+                    ProviderName = $ev.ProviderName
+                    Action = "Up"
+                    Seconds = $diff
+                }
+            }
+            $previous_action = "up"
+            $previous_time = $ev.TimeCreated
+        }
+        elseif ($ev.ProviderName -eq "Eventlog" -and $ev.Id -eq 6005) {
+            [int]$diff = ($ev.TimeCreated - $previous_time).TotalSeconds
+            if (-Not ($previous_action -eq "up" -and ($diff -le $ignore_window))) {
+                [PSCustomObject]@{
+                    TimeCreated = $ev.TimeCreated
+                    Id = $ev.Id
+                    ProviderName = $ev.ProviderName
+                    Action = "Up"
+                    Seconds = $diff
+                }
+            }
+            $previous_action = "up"
+            $previous_time = $ev.TimeCreated
+        }
+        elseif ($ev.ProviderName -eq "Microsoft-Windows-Kernel-General" -and $ev.Id -eq 12) {
+            [int]$diff = ($ev.TimeCreated - $previous_time).TotalSeconds
+            if (-Not ($previous_action -eq "up" -and ($diff -le $ignore_window))) {
+                [PSCustomObject]@{
+                    TimeCreated = $ev.TimeCreated
+                    Id = $ev.Id
+                    ProviderName = $ev.ProviderName
+                    Action = "Up"
+                    Seconds = $diff
+                }
+            }
+            $previous_action = "up"
+            $previous_time = $ev.TimeCreated
+        }
+        elseif ($ev.ProviderName -eq "Microsoft-Windows-Kernel-General" -and $ev.Id -eq 13) {
+            [int]$diff = ($ev.TimeCreated - $previous_time).TotalSeconds
+            if (-Not ($previous_action -eq "down" -and ($diff -le $ignore_window))) {
+                [PSCustomObject]@{
+                    TimeCreated = $ev.TimeCreated
+                    Id = $ev.Id
+                    ProviderName = $ev.ProviderName
+                    Action = "Down"
+                    Seconds = $diff
+                }
+            }
+            $previous_action = "down"
+            $previous_time = $ev.TimeCreated
+        }
+        elseif ($ev.ProviderName -eq "Eventlog" -and $ev.Id -eq 6006) {
+            [int]$diff = ($ev.TimeCreated - $previous_time).TotalSeconds
+            if (-Not ($previous_action -eq "down" -and ($diff -le $ignore_window))) {
+                [PSCustomObject]@{
+                    TimeCreated = $ev.TimeCreated
+                    Id = $ev.Id
+                    ProviderName = $ev.ProviderName
+                    Action = "Down"
+                    Seconds = $diff
+                }
+            }
+            $previous_action = "down"
+            $previous_time = $ev.TimeCreated
+        }
+        elseif ($ev.ProviderName -eq "Eventlog" -and $ev.Id -eq 6008) {
+            [int]$diff = ($ev.TimeCreated - $previous_time).TotalSeconds
+            if (-Not ($previous_action -eq "down" -and ($diff -le $ignore_window))) {
+                [PSCustomObject]@{
+                    TimeCreated = $ev.TimeCreated
+                    Id = $ev.Id
+                    ProviderName = $ev.ProviderName
+                    Action = "Down"
+                    Seconds = $diff
+                }
+            }
+            $previous_action = "down"
+            $previous_time = $ev.TimeCreated
+        }
+        elseif ($ev.ProviderName -eq "Microsoft-Windows-Kernel-Power" -and $ev.Id -eq 107) {
+            [int]$diff = ($ev.TimeCreated - $previous_time).TotalSeconds
+            if (-Not ($previous_action -eq "down" -and ($diff -le $ignore_window))) {
+                [PSCustomObject]@{
+                    TimeCreated = $ev.TimeCreated
+                    Id = $ev.Id
+                    ProviderName = $ev.ProviderName
+                    Action = "Down"
+                    Seconds = $diff
+                }
+            }
+            $previous_action = "down"
+            $previous_time = $ev.TimeCreated
+        }
+        elseif ($ev.ProviderName -eq "Microsoft-Windows-Kernel-Power" -and $ev.Id -eq 42) {
+            [int]$diff = ($ev.TimeCreated - $previous_time).TotalSeconds
+            if (-Not ($previous_action -eq "down" -and ($diff -le $ignore_window))) {
+                [PSCustomObject]@{
+                    TimeCreated = $ev.TimeCreated
+                    Id = $ev.Id
+                    ProviderName = $ev.ProviderName
+                    Action = "Down"
+                    Seconds = $diff
+                }
+            }
+            $previous_action = "down"
+            $previous_time = $ev.TimeCreated
+        }
+    }
+    if ($Details) { $evs3 | Out-GridView }
+
+    $total_seconds = ($evs3 | Where-Object Action -eq "down"| Measure-Object -Sum -Property Seconds).Sum
+    $total_downs = ($evs3 | Where-Object Action -eq "down" | Measure-Object).Count -1
+    if ($pseudo0.Id -eq 13) { $total_downs = $total_downs - 1}
+    [int]$total_hours = $total_seconds/3600
+
+
+    "total seconds: {0:N0}" -f $total_seconds
+    "total hours  : {0:N1}" -f $total_hours
+    "total days : {0:N1}" -f ($total_hours/24)
+    "total of $total_downs shutdown(s) in $Days days"
 }
 else {
-    Write-Host "$Computername is not reachable"
+    Write-Host "$_ is down."
 }
